e8da5a642e12262d1a0bccbee412425bfac9b143
Smack <kovacsmyles1@gmail.com>
2020-12-20 16:23:00 -0600

    code is mostly working, generates passwords but does so infinitely and not to the textbox

--- Develop/readme.md
+++ Develop/readme.md
@@ -12,3 +12,7 @@ Tool to dynamically generate a password based on user inputs and filters.
  https://myleskovacs.github.io/Password-generator/
 
 ![ScreenShot](./Images/liveSite.JPG)
+
+
+## Credit
+Credit to Traversy Media video https://www.youtube.com/watch?v=duNmhKgtcsI helped understand some things for the project and demonstrated some ways I could turn my pseudocode into working code.
--- Develop/script.js
+++ Develop/script.js
@@ -1,63 +1,66 @@
 // Assignment code here
-var generatePasword = function() {
-  var getLength = function() {
-  // Determines the users desired password length
-  var length = "";
-  while (length === "" || length === null) {
-    length = prompt("How long would you like your password to be? Please enter a value between 8 and 128")
-  }
-  console.log(length);
-  return length;
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+var generateFilters = function() { 
+  debugger;
+  const length = +prompt("How long would you like your password? (must be between 8 and 128 characers");
+  const hasLower = window.confirm("Would you like to include lowercase characters?");
+  const hasUpper = window.confirm("Would you like to include uppercase characters?");
+  const hasNumber = window.confirm("Would you like to include numbers?");
+  const hasSymbol = window.confirm("Would you like to include symbols?")
+
+   return generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 };
 
 
-  // Determines if password is using lowercase letters
-  var lower = window.confirm("Would you like to include lowercase?")
-    if (lowerConfirm) {
-      //include lowercase
+var generatePassword = function(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item =>
+Object.values(item)[0]);
+
+    // No selected type
+    if(typesCount === 0) {
+      return '';
     }
-    else {
-      //exclude lowercase
-    };
-  // Determines if password is using uppercase letters
-  var upper = window.confirm("Would you like to include uppercase?")
-    if (upperConfirm) {
-      //include uppercase
+
+    // Loop it
+    for(let i=0; i<generateFilters.length; i+typesCount) {
+      typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+        generatedPassword += randomFunc[funcName] ();
+      });
     }
-    else {
-      //exclude uppercase
-    };
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
 
 
-  // Determines if password is using numbers
-  var numbers = window.confirm("Would you like to include numbers?")
-    if (numbersConfirm) {
-      //include numbers
-    }
-    else {
-      //exclude numbers
-    };
-
-
-  // Determines if password is using special characters
-  var special = window.confirm("Would you like to include special characters?") 
-    if (specialConfirm) {
-      //include special characters
-    }
-    else {
-    //exclude special characters
-    };
-};
-
-generatePassword();
-
-
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+function getRandomNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  generateFilters();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
